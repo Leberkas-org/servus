e383e374336f54e046605de2b5f31cab94d8eb75
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-05-05 10:36:52 +0200

    chore(obsolete): Remove obsolete types

--- src/Servus.Core/Diagnostics/ActivitySourceKeyAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Servus.Core.Diagnostics;
-
-[Obsolete("Will not be supported after migration from Servus.Core to Servus lib.")]
-public class ActivitySourceKeyAttribute(Type sourceKey) : Attribute
-{
-    public Type SourceKey { get; } = sourceKey;
-}
--- src/Servus.Core/Diagnostics/ActivitySourceNameAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Servus.Core.Diagnostics;
-
-[Obsolete("Will not be supported after migration from Servus.Core to Servus lib.")]
-public class ActivitySourceNameAttribute(string name) : Attribute
-{
-    public string ActivitySourceName { get; } = name;
-}
